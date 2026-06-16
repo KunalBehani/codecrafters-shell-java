7cc41aeba8749a43171b1fac48c52e5860adb7a8
Kunal Behani <kunalbehani@Kunals-MacBook-Pro.local>
2026-06-16 23:58:58 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -16,10 +16,19 @@ public class Main {
             if (command.equals("exit")) {
                 break;
             }
-
             if (command.startsWith("echo ")) {
                 System.out.println(command.substring(5));
-            } else {
+            } 
+            else if (command.startsWith("type ")) {
+                String cmd = command.substring(5);
+
+                if (cmd.equals("echo") || cmd.equals("exit") || cmd.equals("type")) {
+                    System.out.println(cmd + " is a shell builtin");
+                } else {
+                    System.out.println(cmd + ": not found");
+                }
+            } 
+            else {
                 System.out.println(command + ": command not found");
             }
         }
