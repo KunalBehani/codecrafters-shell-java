e034c55ea17a2a02a4e25015cba92621ba889f21
Kunal Behani <kunalbehani@Kunals-MacBook-Pro.local>
2026-06-17 00:04:18 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
+import java.io.File;
 import java.util.Scanner;
+import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
+        Set<String> builtins = Set.of("echo", "exit", "type");
 
         while (true) {
             System.out.print("$ ");
@@ -16,18 +20,37 @@ public class Main {
             if (command.equals("exit")) {
                 break;
             }
+
             if (command.startsWith("echo ")) {
                 System.out.println(command.substring(5));
-            } 
+            }
             else if (command.startsWith("type ")) {
                 String cmd = command.substring(5);
 
-                if (cmd.equals("echo") || cmd.equals("exit") || cmd.equals("type")) {
+                if (builtins.contains(cmd)) {
                     System.out.println(cmd + " is a shell builtin");
-                } else {
+                    continue;
+                }
+
+                String pathEnv = System.getenv("PATH");
+                String[] paths = pathEnv.split(File.pathSeparator);
+
+                boolean found = false;
+
+                for (String dir : paths) {
+                    File file = new File(dir, cmd);
+
+                    if (file.exists() && file.isFile() && file.canExecute()) {
+                        System.out.println(cmd + " is " + file.getAbsolutePath());
+                        found = true;
+                        break;
+                    }
+                }
+
+                if (!found) {
                     System.out.println(cmd + ": not found");
                 }
-            } 
+            }
             else {
                 System.out.println(command + ": command not found");
             }
