bad62e2d2ff84e1d7f17f294b467794bfd7fe833
Kunal Behani <kunalbehani@Kunals-MacBook-Pro.local>
2026-06-16 23:46:01 +0530

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,10 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-
+        while (true) {
         System.out.print("$ ");
-
         String command = sc.nextLine();
         System.out.println(command + ": command not found");
-        sc.close();
+        }
     }
 }
